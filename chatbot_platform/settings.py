14577dafffa7679a78f82697b8cd97f3b506cb72
Nandeesha <nandeeshayadav76@gmail.com>
2026-01-14 23:28:11 +0530

    Fix allowed hosts and debug for Render deployment

--- chatbot_platform/settings.py
+++ chatbot_platform/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--06%)g-gd1lm)(frc0q_g_fx*16i11z#dd#9-6!)b=dg)-9#-9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
